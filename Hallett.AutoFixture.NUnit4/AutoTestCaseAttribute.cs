de2dbea0a16614fa662d19bba46aafbda8502133
Tony Hallett <tonyhallett74@gmail.com>
2026-06-29 11:05:16 +0100

    protected internal ctor

--- Hallett.AutoFixture.NUnit4/AutoTestCaseAttribute.cs
+++ Hallett.AutoFixture.NUnit4/AutoTestCaseAttribute.cs
@@ -12,7 +12,7 @@ namespace Hallett.AutoFixture.NUnit4
         public AutoTestCaseAttribute(params object?[] values) : base(values) 
             => TestMethodBuilder = new FixedNameTestMethodBuilderCancellationTokenAware();
 
-        protected AutoTestCaseAttribute(Func<IFixture> fixtureFactory, params object?[] arguments) : base(fixtureFactory, arguments) 
+        protected internal AutoTestCaseAttribute(Func<IFixture> fixtureFactory, params object?[] arguments) : base(fixtureFactory, arguments) 
             => TestMethodBuilder = new FixedNameTestMethodBuilderCancellationTokenAware();
     }
 }
