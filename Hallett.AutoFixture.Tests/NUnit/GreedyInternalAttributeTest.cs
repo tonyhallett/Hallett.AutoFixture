43f5607e57c5bef81cdc80ecf1744a6805b05dab
Tony Hallett <tonyhallett74@gmail.com>
2026-09-22 09:46:44 +0100

    Add test for GreedyInternalAttribute

---
+++ Hallett.AutoFixture.Tests/NUnit/GreedyInternalAttributeTest.cs
@@ -0,0 +1,34 @@
+﻿using Hallett.AutoFixture.NUnit4;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Hallett.AutoFixture.Tests.NUnit
+{
+    
+    internal class GreedyInternalAttributeTest
+    {
+        public class TestClass
+        {
+            internal TestClass() => ThrowIfChosen();
+
+            internal TestClass(int a) => ThrowIfChosen();
+
+            internal TestClass(int a, int b)
+            { }
+
+            public TestClass(int a, int b, int c) => ThrowIfChosen();
+
+            private static void ThrowIfChosen() => throw new InvalidOperationException("This constructor should not be chosen by GreedyInternalAttribute.");
+        }
+
+        [AutoTest]
+        [Test]
+#pragma warning disable IDE0060 // Remove unused parameter
+        public void GreedyInternal_Should_Choose_The_Internal_Constructor_With_Most_Parameters([GreedyInternal] TestClass testClass)
+#pragma warning restore IDE0060 // Remove unused parameter
+        {
+            
+        }
+    }
+}
